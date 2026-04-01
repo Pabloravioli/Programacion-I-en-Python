4fa0be3feab7a60402002345df201c870cb1f9d1
pabloravioli <pabloespinosa888@gmail.com>
2026-04-01 20:14:38 -0300

    Se completo el ejercicio 2

--- Actividad 1 Obligatoria/Ejercicio 2.py
+++ Actividad 1 Obligatoria/Ejercicio 2.py
@@ -1,3 +1,20 @@
 #Ejercicio 2: Dada la siguiente lista numeros=[3,7,10,15,20,25]
 
 #Crear una nueva lista que contenga el doble de cada número. Utilizar listas por comprension
+
+def crear_nueva_lista(lista):
+
+    
+
+    nueva_lista = [numero*2 for numero in lista ]
+
+    return nueva_lista
+
+def main():
+   
+   lista_numeros=[3,7,10,15,20,25] 
+   lista_nueva = crear_nueva_lista(lista_numeros)
+   print(lista_nueva)
+   
+main()
+
