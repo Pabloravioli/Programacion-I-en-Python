ee1f414e22c840d90dc3f0106e9f9889279b9154
pabloravioli <pabloespinosa888@gmail.com>
2026-05-17 23:48:36 -0300

    Ejercicio UVA Archivos

---
+++ Archivos UVA ejercicio 1.py
@@ -0,0 +1,40 @@
+"""
+Crear un programa que permita registrar la información de los libros de una biblioteca, utilizando un archivo para almacenar los datos. El registro de libros incluirá el título, el autor y la fecha de publicación.   
+Adicionalmente, se deberá poder realizar la visualización de cada uno de los libros cargados. 
+"""
+
+
+"""
+Para lograr el objetivo, deberá tener en cuenta los siguientes puntos:
+"""
+
+"""
+Crea un archivo llamado libros.txt para almacenar los datos de los libros. Si el archivo ya existe, no se debe sobrescribir, sino agregar datos al final.
+Crea una función llamada registrarLibro que reciba como parámetros el título del libro, el autor y la fecha de publicación, y guarde esta información en el archivo antes mencionado.
+Se debe asegurar de que cada registro se almacene en una nueva línea del archivo y que los datos estén separados por el carácter ";" (punto y coma). Por ejemplo:
+
+Ser feliz era esto;Eduardo Sacheri;2014 
+
+Steve Jobs (La biografía);Walter Isaacson;2011 
+
+Los años de peregrinación del chico sin color;Haruki Murakami;2013
+"""
+
+"""
+Define una función llamada mostrarLibros, que lea el archivo libros.txt y muestre en pantalla la lista de libros.
+"""
+
+"""
+Crea un bucle principal que permita al usuario seleccionar una opción: registrar un nuevo libro (solicitando el título, el autor y el año de publicación)
+o mostrar el listado de libros.
+El bucle debe repetirse hasta que el usuario decida salir (contemple agregar una nueva opción).
+Utilizar excepciones para controlar que la carga y la generación de los archivos se realice correctamente.
+Además, verifica que el título y el autor del libro sean cadenas de texto no vacías y el año de publicación un valor numérico (éste último utilizando excepciones)
+"""
+
+
+def registrarLibro():
+    try:
+        rutaActual = os.path.dirname(__file__)
+        rutaArchivo = os.path.join(rutaActual,"libros.txt")
+    
