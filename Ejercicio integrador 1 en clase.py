5412e72ecc81e642f1e2cd2d65e455f4b41f91e7
pabloravioli <pabloespinosa888@gmail.com>
2026-05-22 20:24:02 -0300

    Mas ejercicios realizados

---
+++ Ejercicio integrador 1 en clase.py
@@ -0,0 +1,20 @@
+"""
+
+Un archivo notas.txt contiene:
+
+legajo;nombre;nota1;nota2;nota3
+
+Se solicita:
+
+1. Leer el archivo.
+
+2. Guardar en un diccionario el nombre y la lista de notas.
+
+3. Calcular promedio por alumno.
+
+4. Generar promedios.txt.
+
+5. Mostrar alumnos aprobados.
+
+"""
+
---
+++ Ejercicio integrador 2 en clase.py
@@ -0,0 +1,22 @@
+"""
+
+EJERCICIO 4 — REGISTRO DE TEMPERATURAS
+Archivo: ciudad;temperatura
+
+Se solicita:
+
+1. Guardar temperaturas por ciudad.
+
+2. Calcular promedio.
+
+3. Mostrar ciudad más calurosa.
+
+4. Generar temperaturas_extremas.txt dos registros:
+
+ 
+
+ciudad;temperatura_maxima
+
+ciudad;temperatura_minima
+
+"""
